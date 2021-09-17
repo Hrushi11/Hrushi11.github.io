6643dd906c34c42af9f08455f734b844adcde30d
Hrushikesh <hrushiskachgunde@gmail.com>
2021-09-17 15:22:14 +0530

    add src files

---
+++ script.js
@@ -0,0 +1,49 @@
+$(document).ready(function () {
+  /* For the sticky navigation */
+  $(".js--section-features").waypoint(
+    function (direction) {
+      if (direction == "down") {
+        $("nav").addClass("sticky");
+      } else {
+        $("nav").removeClass("sticky");
+      }
+    },
+    {
+      offset: "60px;",
+    }
+  );
+
+  /* Scroll on buttons */
+  $(".js--scroll-to-plans").click(function () {
+    $("html, body").animate(
+      { scrollTop: $(".js--section-projects").offset().top },
+      350
+    );
+  });
+
+  /* Animations on scroll */
+  $(".js--wp-1").waypoint(
+    function (direction) {
+      $(".js--wp-1").addClass("animated fadeIn");
+    },
+    {
+      offset: "50%",
+    }
+  );
+
+  $(".js--wp-2").waypoint(
+    function (direction) {
+      $(".js--wp-2").addClass("animated fadeIn");
+    },
+    {
+      offset: "50%",
+    }
+  );
+
+  /* Mobile navigation */
+  $(".js--nav-icon").click(function () {
+    var nav = $(".js--main-nav");
+
+    nav.slideToggle(200);
+  });
+});
